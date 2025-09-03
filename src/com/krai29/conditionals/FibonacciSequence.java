d7ce7d8e1c6d68aacc13995ba3f36b720833f947
Karan Rai <karanrai@Karans-MacBook-Air.local>
2025-09-03 16:41:00 +0530

    Fibonacci sequence

---
+++ src/com/krai29/conditionals/FibonacciSequence.java
@@ -0,0 +1,24 @@
+package com.krai29.conditionals;
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a = 0;
+        int b = 1;
+        if (n >= 1){
+            System.out.print(a);
+        }
+        if (n>=2){
+            System.out.print(", "+b);
+        }
+        for (int i = 3;i<=n;i++){
+            int sum = a + b;
+            a = b;
+            b = sum;
+            System.out.print(", "+b);
+        }
+    }
+}
