b9de312ef22eb536fe382b28481bba7df2563318
Karan Rai <karanrai@Karans-MacBook-Air.local>
2025-09-11 11:11:57 +0530

    strings basics

---
+++ src/com/krai29/string/PrettyPrinting.java
@@ -0,0 +1,10 @@
+package com.krai29.string;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.123f;
+        System.out.printf("Formatted number is %.2f%n",a);
+        System.out.printf("Value of PI is %.3f%n",Math.PI);
+        System.out.printf("I am %s and I am %s","Karan","Cool");
+    }
+}
