009faa6314dfc15d22e743552215ffc06159c00b
Karan Rai <karanrai@Karans-MacBook-Air.local>
2025-09-20 11:19:34 +0530

    flow example

---
+++ src/com/krai29/kotlin/coroutines/FlowExample.kt
@@ -0,0 +1,19 @@
+package com.krai29.kotlin.coroutines
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.flow
+
+suspend fun main(){                    // or main() = runBlocking{}
+    val helloFlow = flow{
+        emit("Hello")
+        emit("I")
+        emit("am")
+        emit("happy")
+        emit("to")
+        emit("see you")
+    }
+
+    helloFlow.collect{value ->
+        println(value)
+    }
+}
